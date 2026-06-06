184043b8fc29c9562b9cccf5b7f5879e104cffb5
guruindo <nemujayatoko@gmail.com>
2026-06-06 21:40:04 +0700

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -8,7 +8,7 @@ const SUPABASE_URL = 'https://izcaycwgaitalxhwxqkr.supabase.co'
 const SUPABASE_KEY = 'sb_publishable_HuPvZEM2r9DcrMrwkmWJWA_HhkYsoyu'
 // ================================================================
 
-if (SUPABASE_URL === 'https://izcaycwgaitalxhwxqkr.supabase.co' || SUPABASE_KEY === 'sb_publishable_HuPvZEM2r9DcrMrwkmWJWA_HhkYsoyu') {
+if (SUPABASE_URL === 'GANTI_DENGAN_URL_SUPABASEMU' || SUPABASE_KEY === 'GANTI_DENGAN_PUBLISHABLE_KEY') {
   console.warn('⚠️ Buka js/supabase.js dan isi URL dan Key dari Supabase!')
 }
 
